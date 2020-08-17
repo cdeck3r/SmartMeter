34d0b4569afcc17dd9c480cf67c3e8f9d22572fb
cdeck3r <cdecker@outlook.de>
2020-08-17 20:09:43 +0200

    Added file management

--- raspi/fileservice.sh
+++ raspi/fileservice.sh
@@ -22,6 +22,10 @@ DBU_CFG=~/.dropbox_uploader
 IMG_DIR=${SCRIPT_DIR}/images
 DBU_DIR=images
 
+# needed for file management
+REMOTE_FILELST=/tmp/remote_flist.txt
+LOCAL_FILELST=/tmp/local_flist.txt
+BOTH_FILELST=/tmp/both_flist.txt
 
 #####################################################
 # Include Helper functions
@@ -59,3 +63,28 @@ for img_file in "${IMG_DIR}"/*.png; do
     "${DBU}" -f "${DBU_CFG}" -s upload "$img_file" "${DBU_DIR}/${fname}"
 done
 
+####################################
+# File Management
+####################################
+
+# list remote files
+"${DBU}" -f "${DBU_CFG}" list "${DBU_DIR}" | cut -d' ' -f4 | grep -P '^\d{8}_\d{6}\.png' > "${REMOTE_FILELST}"
+
+# list local files
+find "${IMG_DIR}" -type f -printf "%f\n" > "${LOCAL_FILELST}"
+
+# list the files found in both
+comm -12 "${REMOTE_FILELST}" "${LOCAL_FILELST}" > "${BOTH_FILELST}"
+
+# we can now safely remove the local files
+# not very elegant, but it works
+for f in $(cat "${BOTH_FILELST}") ; do 
+  rmfile="${IMG_DIR}"/"$f"
+  log_echo "INFO" "NOT IMPLEMENTED YET - Delete file: ${rmfile}"
+  #rm -rf "${rmfile}"
+done
+
+# cleanup
+rm -rf "${REMOTE_FILELST}"
+rm -rf "${LOCAL_FILELST}"
+rm -rf "${BOTH_FILELST}"
