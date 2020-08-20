85e4a477d0ff59a56232cdaab67f26cbb6b09c35
cdeck3r <cdecker@outlook.de>
2020-08-20 15:14:04 +0200

    Added logrotate

--- raspi/smeter.sh
+++ raspi/smeter.sh
@@ -14,7 +14,9 @@
 # We assume all scripts are in the SCRIPT_DIR
 
 # Params: none
-# Exit codes: none
+# Exit codes: 
+# 0: all ok
+# 1: if script could not be started
 
 # this directory is the script directory
 SCRIPT_DIR="$(
@@ -26,7 +28,8 @@ cd "$SCRIPT_DIR" || exit
 SCRIPT_NAME=$0
 
 # variables
-
+LOG_DIR="${SCRIPT_DIR}"/log
+IMG_DIR="${SCRIPT_DIR}"/images
 
 #####################################################
 # Include Helper functions
@@ -40,14 +43,29 @@ source "${SCRIPT_DIR}/funcs.sh"
 # First things first
 assert_on_raspi
 
+# create necessary directories
+mkdir -p "${IMG_DIR}"
+mkdir -p "${LOG_DIR}"
+
+# here we go
 log_echo "INFO" "SmartMeter camera system starts."
 
 # check maintenance mode
-"${SCRIPT_DIR}"/maintenance.sh
-MAINTENANCE_MODE=$?
+if [ -x "${SCRIPT_DIR}"/maintenance.sh ]; then 
+    "${SCRIPT_DIR}"/maintenance.sh
+    MAINTENANCE_MODE=$?
+else
+    log_echo "ERROR" "Could not start script: ${SCRIPT_DIR}/maintenance.sh"
+    exit 1
+fi
 
 # take picture
-#"${SCRIPT_DIR}"/takepicture.sh
+if [ -x "${SCRIPT_DIR}"/takepicture.sh ]; then 
+    #"${SCRIPT_DIR}"/takepicture.sh
+else
+    log_echo "ERROR" "Could not start script: ${SCRIPT_DIR}/takepicture.sh"
+    exit 1
+fi
 
 # upload to dropbox
 # "${SCRIPT_DIR}"/fileservice.sh
@@ -59,6 +77,14 @@ if [[ $? -eq 2 ]]; then
     if [[ $? -ne 0 ]]; then
         log_echo "ERROR" "Re-occuring network problem. No further retry."
     fi
+fi
+
+# handle logs using logrotate
+if [ -x "${SCRIPT_DIR}"/logrotate.sh ]; then 
+    "${SCRIPT_DIR}"/logrotate.sh
+else
+    log_echo "ERROR" "Could not start script: ${SCRIPT_DIR}/logrotate.sh"
+    exit 1
 fi
 
 # shutdown
