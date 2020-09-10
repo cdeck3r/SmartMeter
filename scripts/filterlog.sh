4ca19fca322e410ce1d9f7286f95942888bac638
cdeck3r <cdecker@outlook.de>
2020-09-10 11:30:14 +0200

    Completed logfile analysis

--- scripts/filterlog.sh
+++ scripts/filterlog.sh
@@ -21,7 +21,11 @@ cd "$SCRIPT_DIR" || exit
 SCRIPT_NAME=$0
 
 # variables
-LOG_DIR="${SCRIPT_DIR}"
+# ... for download
+WORK_DIR="/tmp/smartmeterlog"
+LOG_DROPBOX_URL='https://www.dropbox.com/sh/esiznlynqklmcjm/AAAsmTOiGAKCucCsnWQdoeTpa?dl=0'
+# ... for processing
+LOG_DIR="${WORK_DIR}"
 LOG_FILES="smartmeter.log-*.bz2"
 
 
@@ -35,6 +39,23 @@ LOG_FILES="smartmeter.log-*.bz2"
 # Main program
 #####################################################
 
+
+# ensure WORK_DIR exists and is empty
+mkdir -p "${WORK_DIR}" || exit
+# save current dir on stack
+pushd "${WORK_DIR}" >/dev/null
+cd "${WORK_DIR}" || exit
+rm -rf "${WORK_DIR}"/*
+
+# download log.zip
+wget --no-parent -nH -nd --content-disposition "${LOG_DROPBOX_URL}"
+
+# unzip
+unzip -qq log.zip
+
+# return to current dir from stack
+popd >/dev/null
+
 for logfile in $(find "${LOG_DIR}" -type f -name "${LOG_FILES}")
 do
     # filter log
@@ -45,5 +66,8 @@ do
     # 5. replace the first 4 occurances of blanks by ; as new field separator 
     bzcat "${logfile}" | tr -s '[:blank:]' | tr ';' '.' | cut -d' ' -f1,2,4,6,8- | grep -P '^\d{4}-\d{2}-\d{2}' | sed 's/\s/;/' | sed 's/\s/;/' | sed 's/\s/;/' | sed 's/\s/;/'
 done
+
+# cleanup
+rm -rf "${WORK_DIR}"
 
 
