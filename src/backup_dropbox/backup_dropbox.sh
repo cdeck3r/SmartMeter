be1a8e56f85047cd5e8ad3c9755efe989f8bec0c
cdeck3r <cdecker@outlook.de>
2020-09-27 13:47:09 +0200

    Backup images from dropbox

---
+++ src/backup_dropbox/backup_dropbox.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+#
+# Backup images from Dropbox
+# 
+# Author: cdeck3r
+#
+
+# Params: none
+
+# Exit codes
+# 1: pre-cond not fulfilled
+# 2: network or host problem
+
+# this directory is the script directory
+SCRIPT_DIR="$(
+    cd "$(dirname "$0")" || exit
+    pwd -P
+)"
+cd "$SCRIPT_DIR" || exit
+# shellcheck disable=SC2034
+SCRIPT_NAME=$0
+
+# variables
+DBU=${SCRIPT_DIR}/dropbox_uploader.sh
+DBU_CFG=/SmartMeterRepo/.dropbox_uploader # File is not part of the Repo
+IMG_DIR=/SmartMeterRepo/images
+DBU_DIR=images
+
+# needed for file management
+REMOTE_FILELST=/tmp/remote_flist.txt
+LOCAL_FILELST=/tmp/local_flist.txt
+BOTH_FILELST=/tmp/both_flist.txt
+
+#####################################################
+# Include Helper functions
+#####################################################
+
+source "${SCRIPT_DIR}/funcs.sh"
+
+#####################################################
+# Main program
+#####################################################
+
+# first things first
+assert_in_docker
+
+# check connection to dropbox.com
+curl -L dropbox.com >/dev/null 2>&1 && { log_echo "INFO" "Website dropbox.com is reachable."; } || { log_echo "ERROR" "Error retrieving dropbox.com.  Abort."; exit 2; }
+
+# Dropbox Uploader exists?
+if [ ! -f "${DBU}" ]; then
+    log_echo "ERROR" "Abort. Dropbox Uploader not found: ${DBU}"
+    exit 1
+fi
+# Dropbox Uploader executable?
+if [ ! -x "${DBU}" ]; then
+    log_echo "WARN" "Dropbox Upload not executable. Will make executable: ${DBU}"
+    chmod u+x "${DBU}"
+fi
+# directory exists?
+if [ ! -d "${IMG_DIR}" ]; then
+    log_echo "ERROR" "Abort. Image dir does not exist: ${IMG_DIR}"
+    exit 1
+fi
+
+# download all image list
+log_echo "INFO" "Download image list: ${REMOTE_FILELST}"
+"${DBU}" -f "${DBU_CFG}" list "${DBU_DIR}" | sed 's/\s\s*/ /g' | cut -d' ' -f3,4 | grep -P '^\d+\s\d{8}_\d{6}\.png' | sort > "${REMOTE_FILELST}"
+
+# test number of files on Dropbox
+remote_flist_count=$(cat "${REMOTE_FILELST}" | wc -l)
+if [ "$remote_flist_count" -eq 0 ]; then
+    log_echo "INFO" "No files found on Dropbox. Stop."
+    exit 0
+fi
+
+# download all images  
+for img_file in $(cat "${REMOTE_FILELST}" | cut -d' ' -f2); do
+    log_echo "INFO" "Download from Dropbox: ${img_file}"
+    "${DBU}" -f "${DBU_CFG}" download "${DBU_DIR}/${img_file}" "${IMG_DIR}/${img_file}"
+done
+
+####################################
+# File Management
+####################################
+
+# we have downloaded the remote list previously
+# ${REMOTE_FILELST}
+
+# list local files
+find "${IMG_DIR}" -type f -printf "%s %f\n" | sort > "${LOCAL_FILELST}"
+
+# list the files found in both
+comm -12 "${REMOTE_FILELST}" "${LOCAL_FILELST}" > "${BOTH_FILELST}"
+
+# we can now safely remove the remote files
+# not very elegant, but it works
+for f in $(cat "${BOTH_FILELST}" | cut -d' ' -f2) ; do 
+  rmfile="${DBU_DIR}"/"$f"
+  log_echo "INFO" "Delete file on Dropbox: ${rmfile}"
+  "${DBU}" -f "${DBU_CFG}" delete "${rmfile}"
+done
+
+# cleanup
+rm -rf "${REMOTE_FILELST}"
+rm -rf "${LOCAL_FILELST}"
+rm -rf "${BOTH_FILELST}"
---
+++ src/backup_dropbox/funcs.sh
@@ -0,0 +1,61 @@
+#
+# Some common functions
+#
+# Author: cdecke3r
+#
+
+#
+# logging on stdout
+# Param #1: log level, e.g. INFO, WARN, ERROR
+# Param #2: log message
+log_echo () {
+    LOG_LEVEL=$1
+    LOG_MSG=$2
+    TS=$(date '+%Y-%m-%d %H:%M:%S,%s')
+    echo "$TS - $SCRIPT_NAME - $LOG_LEVEL - $LOG_MSG"
+}
+
+# no args
+assert_on_raspi () {
+    # check we are on Raspi
+    MACHINE=$(uname -m)
+    if [[ "$MACHINE" != arm* ]]; then
+        log_echo "ERROR" "We are not on an arm plattform: ${MACHINE}"
+        exit 1
+    fi 
+}
+
+#
+# assert docker
+# we expect the script to execute within the docker container
+assert_in_docker() {
+	# Src: https://stackoverflow.com/a/20012536
+	grep -Eq '/(lxc|docker)/[[:xdigit:]]{64}' /proc/1/cgroup
+
+	if [ "$?" -ne "0" ]; then
+		log_echo "ERROR" "Please run this script in docker container"
+		exit 1
+	fi
+
+}
+#
+# Sleep routines
+# 
+
+# Computes the now epoch timestamp mod a given number of seconds
+# Ex. given 3600 as parameter, it computes the now timestamp modulo 3600
+# and yields the number of seconds within the current hour.
+#
+# Param #1: seconds
+now_sec_modulo () {
+    local SECS=$1
+    echo $(( $(date +%s) % $SECS ))
+}
+
+sec_in_current_quarter () {
+    echo $(now_sec_modulo 900)
+}
+
+sec_until_next_quarter () {
+    echo $(( 900 - $(sec_in_current_quarter) ))
+}
